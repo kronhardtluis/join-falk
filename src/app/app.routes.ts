bb552163f05952ffe260444e06eec0a1e2173780
kronhardtluis <kronhardtluis@gmail.com>
2026-02-16 15:29:12 +0100

    Erstellung des Projekts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Mainpage } from './mainpage/mainpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: Mainpage},
+// {path:'legal-notice', component:},
+// {path:'privacy-policy', component:}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Navbar } from './shared/navbar/navbar';
+import { Mainpage } from './mainpage/mainpage';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
