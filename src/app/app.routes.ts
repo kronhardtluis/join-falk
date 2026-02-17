5af7f002bd8e1a30195c51b3d04b7f4bd19ce588
ArturGr <arturgroblicki@outlook.de>
2026-02-17 17:13:39 +0100

    Added basic list of contacts to contact component. Added router link for contact.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
+import { Contact } from './mainpage/contact/contact';
 
 export const routes: Routes = [
   {path:'', component: Mainpage},
 // {path:'legal-notice', component:},
-// {path:'privacy-policy', component:}
+// {path:'privacy-policy', component:},
+  {path: 'contacts', component: Contact}
 ];
--- src/app/mainpage/contact/contact.ts
+++ src/app/mainpage/contact/contact.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { inject, signal } from '@angular/core';
+import { Supabase } from '../../services/supabase';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss',
 })
 export class Contact {
+
+  public selectedContactId = signal<number | null>(null);
+
+ //Hier binden wir den Datenbankdienst ein.
+  dbService = inject(Supabase);
+  //Hier laden wir Daten vom Server herunter.
+  ngOnInit(){
+    this.dbService.getContacts();
+    this.dbService.subscribeToContactsChanges();
+  }
+
+  /**
+  * Gets the first letter of the second word (usually the last name) from a string.
+  * @param {string} name - The full name string.
+  * @returns {string} The second initial or an empty string if not found.
+  */
+  getSecondInitial(name: string): string {
+    const parts = name.trim().split(/\s+/);
+    return parts.length > 1 ? parts[1][0].toUpperCase() : '';
+  }
+
+  openAddDialogWindow(){
+    console.log('Opened add dialog window');
+  }
+
+  openDetailDialogWindow(id: number | undefined){
+    console.log("Opened contact detail with id:" + id)
+    if(id) this.selectedContactId.set(id);
+  }
 
 }
