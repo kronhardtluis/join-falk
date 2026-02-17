9a49920a9e9bd7581b6aa64fe7dc0993532c2441
ferazasghar6-max <ferazasghar6@gmail.com>
2026-02-17 17:07:53 +0100

    summary component addet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
 import { Contact } from './mainpage/contact/contact';
+import { Summary } from './mainpage/summary/summary';
 
 export const routes: Routes = [
   {path:'', component: Mainpage},
 {path:'contacts', component:Contact},
-// {path:'privacy-policy', component:}
+{path:'summary', component:Summary}
 ];
