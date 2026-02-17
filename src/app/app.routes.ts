473345171546e8885c7efa9b89f8af1dc0fb1ead
kronhardtluis <kronhardtluis@gmail.com>
2026-02-17 16:44:29 +0100

    add-task created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
+import { AddTask } from './mainpage/add-task/add-task';
 
 export const routes: Routes = [
   {path:'', component: Mainpage},
+  {path:'add-task', component:  AddTask}
 // {path:'legal-notice', component:},
 // {path:'privacy-policy', component:}
 ];
