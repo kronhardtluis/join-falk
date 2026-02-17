48f4f0683fd5a01f1ee35c6d1beb89764eddc858
ArturGr <arturgroblicki@outlook.de>
2026-02-17 10:07:24 +0100

    Added subscription (tracking changes on the database side) and a method for displaying a single contact.

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from './../../../node_modules/@supabase/supabase-js/src/index';
+import { createClient, RealtimeChannel } from '@supabase/supabase-js';
 import { Injectable, signal } from '@angular/core';
 import { environment } from '../environments/environment.example';
 
@@ -19,7 +19,7 @@ export class Supabase {
   private superbaseURL = environment.supabaseUrl;
   private supabaseKEY = environment.supabaseKey;
   private supabase = createClient(this.superbaseURL, this.supabaseKEY);
-
+  channels: RealtimeChannel | undefined;
   contacts = signal<Contact[]>([]);
 
   /**
@@ -30,9 +30,52 @@ export class Supabase {
       .from('contacts')
       .select('*')
       .order('name', { ascending: true });
-    if (data) {
-      this.contacts.set(data);
+    if (data) this.contacts.set(data);
+  }
+
+  /**
+  * Establishes a real-time subscription to the 'contacts' table.
+  * Automatically calls getContacts() whenever an INSERT, UPDATE, or DELETE event occurs
+  * to ensure the local UI state is synchronized with the database.
+  * @returns {void}
+  */
+  subscribeToContactsChanges(){
+    this.channels = this.supabase.channel('custom-all-channel')
+      .on(
+        'postgres_changes',
+        {event: '*', schema: 'public', table: 'contacts'},
+        () => {
+          this.getContacts();
+        }
+      )
+      .subscribe();
+  }
+
+  /**
+  * Lifecycle hook that cleans up resources before the service or component is destroyed.
+  * Unsubscribes from the active Supabase real-time channel to prevent memory leaks.
+  * @returns {void}
+  */
+  ngOnDestroy(){
+    if(this.channels) this.supabase.removeChannel(this.channels);
+  }
+
+  /**
+  * Fetches a single contact from the database by its unique identifier.
+  * @param {number} id - The ID of the contact to retrieve.
+  * @returns {Promise<Contact | null>} A promise that resolves to the contact object or null if not found.
+  */
+  async getContactById(id: number) {
+    const { data, error } = await this.supabase
+      .from('contacts')
+      .select('*')
+      .eq('id', id)
+      .single();
+    if (error) {
+      console.error('Error fetching contact:', error.message);
+      return null;
     }
+    return data;
   }
 
   /**
@@ -57,7 +100,6 @@ export class Supabase {
       .from('contacts')
       .update(contact)
       .eq('id', id);
-
     if (!error) await this.getContacts();
   }
 
@@ -70,8 +112,6 @@ export class Supabase {
       .from('contacts')
       .delete()
       .eq('id', id);
-
     if (!error) await this.getContacts();
   }
-
 }
