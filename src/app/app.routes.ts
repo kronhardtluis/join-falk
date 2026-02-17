096b50f576a4235c9e843b0f63f9d6f1a86ddf63
ferazasghar6-max <ferazasghar6@gmail.com>
2026-02-17 16:14:46 +0100

    Header and Navbar completed, links has to be addet when done.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Mainpage } from './mainpage/mainpage';
+import { Contact } from './mainpage/contact/contact';
 
 export const routes: Routes = [
   {path:'', component: Mainpage},
-// {path:'legal-notice', component:},
+{path:'contacts', component:Contact},
 // {path:'privacy-policy', component:}
 ];
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  activeNav: string = ''; 
 
+  setActiveNav(id: string) {
+    this.activeNav = id;
+  }
 }
