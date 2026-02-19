58af2991c71cc01a3af1feb573f8d0f99f02d4cc
ferazasghar6-max <ferazasghar6@gmail.com>
2026-02-19 11:00:55 +0100

    greeting addet

--- src/app/mainpage/summary/summary.ts
+++ src/app/mainpage/summary/summary.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
@@ -8,5 +8,21 @@ import { RouterLink } from "@angular/router";
   styleUrl: './summary.scss',
 })
 export class Summary {
-  
+  greeting: string = '';
+
+  ngOnInit() {
+    this.setGreeting();
+    setInterval(() => this.setGreeting(), 60000); // jede Minute prüfen
+  }
+
+  setGreeting() {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) {
+      this.greeting = 'Good morning';
+    } else if (hour >= 12 && hour < 18) {
+      this.greeting = 'Good afternoon';
+    } else {
+      this.greeting = 'Good evening';
+    }
+  }
 }
