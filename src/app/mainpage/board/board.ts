942346dccd98df393bf7fd1cfa0dd378ad599102
ferazasghar6-max <ferazasghar6@gmail.com>
2026-02-26 19:01:05 +0100

    board component, work in progress

--- src/app/mainpage/board/board.ts
+++ src/app/mainpage/board/board.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AddTask } from '../add-task/add-task';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [AddTask],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
 export class Board {
-  // Logik und Eigenschaften
-  title = 'Board';
-  columns: string[] = ['To Do', 'In Progress', 'Await Feedback', 'Done'];
+
+@ViewChild('taskDialog') dialog!: ElementRef<HTMLDialogElement>;
+
+  openDialog() {
+    this.dialog.nativeElement.showModal();
+  }
+
+  closeDialog() {
+    this.dialog.nativeElement.close();
+  }
+
 }
