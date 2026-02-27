31dff22b2fd61dad3d03f22c8dfcf5d5a0e83924
kronhardtluis <kronhardtluis@gmail.com>
2026-02-27 10:14:57 +0100

    board-cards edited

--- src/app/mainpage/board/board.ts
+++ src/app/mainpage/board/board.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 
+interface cardTemplate {
+  type: string;
+  title: string;
+  description: string;
+  subtasks: number;
+  doneSubtasks: number;
+  contacts: string[];
+}
+
 @Component({
   selector: 'app-board',
   imports: [],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
+
 export class Board {
   // Logik und Eigenschaften
   title = 'Board';
   columns: string[] = ['To Do', 'In Progress', 'Await Feedback', 'Done'];
+  cards: cardTemplate[] = [
+    {
+      type: 'Technical Task',
+      title: 'Implementierung',
+      description: 'Ein sicheres Login-System für die Benutzer erstellen.',
+      subtasks: 3,
+      doneSubtasks: 1,
+      contacts: ['MT', 'LA', 'AT'],
+    },
+  ];
 }
