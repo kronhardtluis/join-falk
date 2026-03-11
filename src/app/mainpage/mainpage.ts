bd37aa4cc84593c84b743468e9831e2cd0d03d60
ferazasghar6-max <ferazasghar6@gmail.com>
2026-03-11 22:10:30 +0100

    password visibility and form in progress

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
@@ -9,27 +9,35 @@ import { RouterLink } from "@angular/router";
   styleUrl: './mainpage.scss',
 })
 export class Mainpage {
-  
-    // new FormGroup erlaubt uns neue, zusammenhängende Inputfelder anzulegen. --> ReactiveFormsModule
-  userForm = new FormGroup({
-    name: new FormControl('', {
-      // Nach dem Komma validators: [] kommen unsere Validatoren rein.
-      // Validators gefolgt vom Punkt, dann sehen wir, was es alles zur Auswahl gibt.
-      validators: [Validators.required],
-    }),
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.email],
-    }),
+  userForm = new FormGroup(
+    {
+      name: new FormControl('', {
+        // Nach dem Komma validators: [] kommen unsere Validatoren rein.
+        // Validators gefolgt vom Punkt, dann sehen wir, was es alles zur Auswahl gibt.
+        validators: [Validators.required],
+      }),
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+      }),
       password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(8)],
-    }),
+        validators: [Validators.required, Validators.minLength(8)],
+      }),
       confirm: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(8)],
-    }),
-    checkBox: new FormControl(false, {
-    validators: [Validators.requiredTrue], 
-  }),
-  });
+        validators: [Validators.required, Validators.minLength(8)],
+      }),
+      checkBox: new FormControl(false, {
+        validators: [Validators.requiredTrue],
+      }),
+    },
+    {
+      // Custom Validator für den Passwort-Abgleich
+      validators: (group) => {
+        const password = group.get('password')?.value;
+        const confirm = group.get('confirm')?.value;
+        return password === confirm ? null : { mismatch: true };
+      },
+    },
+  );
 
   formSubmit() {
     if (this.userForm.valid) {
@@ -42,8 +50,30 @@ export class Mainpage {
   }
 
   toggleCheckBox() {
-  const currentValue = this.userForm.get('checkBox')?.value;
-  this.userForm.get('checkBox')?.setValue(!currentValue);
-  this.userForm.get('checkBox')?.markAsTouched(); // Damit Fehlermeldungen getriggert werden
-}
+    const currentValue = this.userForm.get('checkBox')?.value;
+    this.userForm.get('checkBox')?.setValue(!currentValue);
+    this.userForm.get('checkBox')?.markAsTouched(); // Damit Fehlermeldungen getriggert werden
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirm = form.get('confirm')?.value;
+
+    // Wenn sie nicht gleich sind, setzen wir einen Fehler "mismatch"
+    return password === confirm ? null : { mismatch: true };
+  }
+
+  handleDisabledClick() {
+    if (this.userForm.invalid) {
+      // Das lässt alle Fehlermeldungen und roten Ränder aufleuchten
+      this.userForm.markAllAsTouched();
+    }
+  }
+
+  isPasswordVisible = false;
+
+  togglePassword(event: MouseEvent) {
+    event.preventDefault(); // Verhindert, dass der Input den Fokus verliert
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
 }
