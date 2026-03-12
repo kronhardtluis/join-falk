b24bc50a0274e87b1ef392c315adff02a76b0120
ferazasghar6-max <ferazasghar6@gmail.com>
2026-03-12 17:04:56 +0100

    help component complete, routerlink fixed

--- src/app/mainpage/help/help.ts
+++ src/app/mainpage/help/help.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-help',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Help {
 
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
